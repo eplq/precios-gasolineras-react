3ddfff2e22ada447ac661ae3fcf80013b487a955
Enrique Pavón <91148468+eplq@users.noreply.github.com>
2022-03-24 14:08:42 +0100

    hay cosas sin terminar aun, pero funciona bien

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -1,16 +1,39 @@
 import { Marker, Popup, useMap } from "react-leaflet";
 import { Icon, polygon as makePolygon } from "leaflet";
+import { useEffect, useState } from "react";
 
 import markerImg from 'leaflet/dist/images/marker-icon.png';
-const icon = new Icon({ iconUrl: markerImg, iconAnchor: [25/2, 41]});
 //                                              x axis middle, y axis bottom
 
 export default function Markers(props) {
     const map = useMap();
+    const [gasolinerasPreciosNormalizados, setGasolinerasPreciosNormalizados] = useState([]);
+    const [minPrice, setMin] = useState(0);
+    const [maxPrice, setMax] = useState(0);
 
-    let markers = props.gasolineras.map((element, index) => {
+    useEffect(() => {
+        const gasolinerasMap = props.gasolineras.map(element => {
+            element["PrecioProducto"] = parseFloat(element["PrecioProducto"].replace(",", "."));
+            return element;
+        });
+        setGasolinerasPreciosNormalizados(gasolinerasMap);
+    }, [props.gasolineras, setGasolinerasPreciosNormalizados]);
+
+    useEffect(() => {
+        if (gasolinerasPreciosNormalizados.length > 0) {
+            setMin(Math.min(...gasolinerasPreciosNormalizados.map(element => element['PrecioProducto'])));
+            setMax(Math.max(...gasolinerasPreciosNormalizados.map(element => element['PrecioProducto'])));
+        }
+    }, [setMin, setMax, gasolinerasPreciosNormalizados]);
+
+    let markers = gasolinerasPreciosNormalizados.map((element, index) => {
         let position = [element['Latitud'].replace(",", "."), element['Longitud (WGS84)'].replace(",", ".")];
-        // 25x41
+
+        let icon = new Icon({
+            iconUrl: markerImg,
+            iconAnchor: [25/2, 41] // 25x41
+        });
+
         return (
             <Marker key={index} position={position} icon={icon}>
                 <Popup>
@@ -26,7 +49,7 @@ export default function Markers(props) {
             color: 'transparent',
             interactive: false
         }
-    ); // trick for getting the center between n points
+    ); // trick for getting the center between n points, buggy sometimes
 
     if (props.gasolineras.length > 0) {
         let center = polygon.addTo(map).getCenter();
