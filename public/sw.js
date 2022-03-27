862a42f97135eec92b25e2273a8481c8cf24cb84
eplq <91148468+eplq@users.noreply.github.com>
2022-03-27 10:02:44 +0200

    pwa

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('fetch', function() {
+    // Para los requisitos de PWA
+})
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './components/App';
 import "./css/index.css";
 import "leaflet/dist/leaflet.css";
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js');
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
